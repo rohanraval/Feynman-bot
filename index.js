0e2e68ebbad83b0975a06bd02e3d082a793ce1b3
Rohan Raval <rohanraval@gmail.com>
2017-05-03 17:23:32 -0400

    init

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
+	res.send('691869293')
 })
 
 // for Facebook verification
